aacbe921faab1194d793e475c45cb7e617a76d58
Lui-G <l.a.maduro@tudelft.nl>
2018-11-07 13:23:24 +0100

    my second commit

--- test.py
+++ test.py
@@ -3,25 +3,25 @@
 
 
 import numpy as np
-print("Hello World!")
+print("Hello World! Poop")
 
 
-#Version 2018-11-07 11:14
-# Made the function circumz(r_1) for circumference
+#####Version 2018-11-07 11:14
+##### Made the function circumz(r_1) for circumference
 
-import numpy as np
-#print("Hello World!")
+#####import numpy as np
+#####print("Hello World!")
 
-r = 5
-circum = 2* np.pi* r
-print(circum)
+#####r = 5
+#####circum = 2* np.pi* r
+#####print(circum)
 
-def circumz(r_1):
-	return  2 * np.pi * r_1
-print(circumz(3))
-	
+#####def circumz(r_1):
+#####	return  2 * np.pi * r_1
+#####print(circumz(3))
+#####	
 
-def AreaCirc(r_2):
-	return np.pi * r_2 ** 2
-print ("The area of a circle of radius 2 is " + str(AreaCirc(2)))
+#####def AreaCirc(r_2):
+#####	return np.pi * r_2 ** 2
+#####print ("The area of a circle of radius 2 is " + str(AreaCirc(2)))
 
